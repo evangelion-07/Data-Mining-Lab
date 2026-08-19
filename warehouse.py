518e784e9216d077d83af881883810eee6c197b7
fangyuan07 <satheeshannarapu@gmail.com>
2026-08-19 15:35:39 +0530

    Update warehouse.py

--- warehouse.py
+++ warehouse.py
@@ -1,7 +1,4 @@
 import pandas as pd
-
-# Data Cube Construction - OLAP Operations
-
 data = {
     "Year": [2024, 2024, 2024, 2025, 2025, 2025],
     "Product": ["Laptop", "Mobile", "Laptop", "Mobile", "Laptop", "Mobile"],
@@ -9,14 +6,10 @@ data = {
                  "Chennai", "Hyderabad", "Chennai"],
     "Sales": [50000, 30000, 75000, 35000, 60000, 40000]
 }
-
-# Create DataFrame
 df = pd.DataFrame(data)
-
 print("Original Data:")
 print(df)
 
-# Data Cube
 cube = pd.pivot_table(
     df,
     values="Sales",
@@ -28,11 +21,6 @@ cube = pd.pivot_table(
 
 print("\nData Cube:")
 print(cube)
-
-# --------------------------------
-# OLAP Operation 1: Roll-Up
-# --------------------------------
-
 print("\nRoll-Up (Year and Product):")
 
 rollup = df.groupby(
@@ -40,11 +28,6 @@ rollup = df.groupby(
 )["Sales"].sum()
 
 print(rollup)
-
-# --------------------------------
-# OLAP Operation 2: Drill-Down
-# --------------------------------
-
 print("\nDrill-Down:")
 
 drill_down = df.groupby(
@@ -52,23 +35,10 @@ drill_down = df.groupby(
 )["Sales"].sum()
 
 print(drill_down)
-
-# --------------------------------
-# OLAP Operation 3: Slice
-# --------------------------------
-
 print("\nSlice (Year = 2025):")
-
 slice_data = df[df["Year"] == 2025]
-
 print(slice_data)
-
-# --------------------------------
-# OLAP Operation 4: Dice
-# --------------------------------
-
 print("\nDice:")
-
 dice_data = df[
     (df["Product"].isin(["Laptop", "Mobile"])) &
     (df["Location"].isin(["Hyderabad", "Chennai"]))
